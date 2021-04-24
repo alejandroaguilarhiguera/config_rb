3731883e56fa9646f50d44c26ecddc13c39f63a9
alejandro aguilar higuera <aaguilar@adhocti.com>
2021-04-24 02:44:32 -0600

    Se agrega servidor para guardar redes inalambricas

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-import bluetooth
+from flask import Flask, request
+from wireless import Wireless
 
-host = ""
-port = 1
-server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-print('Start socket Bluetooth')
-try:
-	server.bind((host, port))
-	print("Binding completed")
-except:
-	print("Binding incompleted")
+wire = Wireless()
+# Funciona !!!
+#print(wire.connect(ssid='Ubee784A-2.4G',password='B5D7F6784A'))
+#print(wire.current())
+#print(wire.interfaces())
 
-server.listen(1)
-client, address = server.accept()
-print("connected:", address)
-print("client:", client)
-try:
-	while True:
-		data = client.recv(1024).decode("utf-8") 
-		print(data)
-		if data == 'a':
-			print('Usted imprimió a')
-		elif data == 'b':
-			print("Usted imprimió b")
-		else:
-			print("Solo puede enviar a o b")
-except KeyboardInterrupt:
-	print('salio')
+app = Flask(__name__)
+
+@app.route('/wireless', methods=['POST'])
+def addWireless():
+    request_data = request.get_json()
+    essid = None
+    password = None
+    #TODO: Intentar conectar
+    if request_data:
+        if 'essid' in request_data:
+            essid = request_data['essid']
+        if 'password' in request_data:
+            password = request_data['password']
+    # wire.connect(ssid=essid, password=password)
+    return request_data
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
